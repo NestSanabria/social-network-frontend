f8278140a3f2c42089fa04da442c71df1a98c16a
Nestor Sanabria <nestsanabria@outlook.com>
2024-10-25 20:09:59 -0500

    Fix script path in index.html for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/social-network-frontend/', // Asegúrate de que este sea tu repositorio
 })
