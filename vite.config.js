ee40d34e7a686c54bd197c72f2546468d5c476a7
Nestor Sanabria <nestsanabria@outlook.com>
2024-10-25 20:31:59 -0500

    Updated configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/social-network-frontend/', // Asegúrate de que este sea tu repositorio
-})
+  base: '/social-network-frontend/', // Asegúrate de tener esta línea
+  build: {
+    rollupOptions: {
+      input: './src/main.jsx',
+      output: {
+        entryFileNames: 'index-[hash].js',
+      }
+    }
+  }
+});
