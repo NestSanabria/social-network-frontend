8cb612b054c56e1fbd335e8493806124cd5db47e
Nestor Sanabria <nestsanabria@outlook.com>
2024-10-25 21:35:58 -0500

    Deploying latest build to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '/social-network-frontend/',
   plugins: [react()],
-  base: '/social-network-frontend/', // Asegúrate de tener esta línea
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx',
-      output: {
-        entryFileNames: 'index-[hash].js',
-      }
-    }
-  }
-});
+})
