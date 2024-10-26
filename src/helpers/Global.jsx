fc9a7b32868a2ab2527adb17e696073fb8f704ed
Nestor Sanabria <nestsanabria@outlook.com>
2024-10-25 19:16:48 -0500

    Component files, paths and other settings

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,10 @@
+//  export const Global = {
+//    url: "https://deploy-backend-api-socialnet.onrender.com/api/"
+//  }
+
+
+
+
+export const Global = {
+  url: "http://localhost:3900/api/"
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Importar assets (recursos: hojas de estilo, imágens, fuentes, etc.)
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
+import './assets/css/normalize.css';
+import './assets/css/styles.css';
+import './assets/css/responsive.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
     <App />
-  </StrictMode>,
+
 )
