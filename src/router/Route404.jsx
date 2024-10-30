1781baff313a95407492e29779aea804e06f8711
Nestor Sanabria <nestsanabria@outlook.com>
2024-10-30 17:05:01 -0500

    Added vercel.json file to handle routes

--- src/router/Route404.jsx
+++
@@ -1,9 +0,0 @@
-// src/router/Error404.jsx
-import { Route } from 'react-router-dom';
-import { Error404 } from '../components/layout/Error404';
-
-const ErrorRoutes = () => (
-  <Route path="*" element={<Error404 />} />
-);
-
-export default ErrorRoutes;
